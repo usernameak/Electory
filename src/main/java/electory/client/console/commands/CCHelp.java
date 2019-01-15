9164593ca09d89ced031fb24d6fd56ce42f8fae3
PROPHESSOR <xenteze@gmail.com>
2019-01-15 03:08:47 +0200

    help по команде
    * Теперь команда help может принимать аргументом название команды и выдавать помощь по ней

--- src/main/java/electory/client/console/commands/CCHelp.java
+++ src/main/java/electory/client/console/commands/CCHelp.java
@@ -10,19 +10,33 @@ public class CCHelp extends ConsoleCommand {
 	private LinkedList<ConsoleCommand> commands;
 
 	public CCHelp(TinyCraft tc, GuiConsole console, LinkedList<ConsoleCommand> commands) {
-		super(tc, console, "help", "Help about the commands and about the command");
+		super(tc, console, "help", "Show this menu. Use help <command> to get help about the command.");
 		this.commands = commands;
+		this.arguments = new String[][] { { "command", "Show help about specific command" } };
 	}
 
 	public void run(String[] arguments) {
-		// TODO: Help about the command
+		if (arguments.length == 0) {
+			console.println("Command list:");
 
-		console.println("Command list:");
+			for (ConsoleCommand command : commands) {
+				console.println("    " + command.getName() + ": " + command.getUsage());
+			}
+		} else {
+			for (ConsoleCommand command : commands) {
+				if (command.getName().equals(arguments[0])) {
+					console.println("Command \"" + command.getName() + "\":" + command.getUsage());
+					console.println("Arguments:");
 
-		for (ConsoleCommand command : commands) {
-			String line = "    " + command.getName() + ": " + command.getUsage();
+					for (String[] arg : command.getArguments()) {
+						console.println("    " + arg[0] + ": " + arg[1]);
+					}
 
-			console.println(line);
+					return;
+				}
+			}
+
+			console.println("Command \"" + arguments[0] + "\" doesn't exist!");
 		}
 	};
 }
