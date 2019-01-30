dd175a0ca32f78218b242e34382c9085c30708b7
usernameak <sitemakers.3dn.ru@gmail.com>
2019-01-30 17:13:32 +0200

    Changed appearance of inventory cells

--- src/main/java/electory/client/gui/widget/GuiWidgetInventoryCell.java
+++ src/main/java/electory/client/gui/widget/GuiWidgetInventoryCell.java
@@ -16,6 +16,8 @@ public class GuiWidgetInventoryCell extends GuiWidget {
 
 	private IContainerProvider container;
 	private int slot;
+	
+	private boolean isHovered = false;
 
 	public GuiWidgetInventoryCell(TinyCraft tc, IContainerProvider container, int slot) {
 		super(tc);
@@ -25,25 +27,29 @@ public class GuiWidgetInventoryCell extends GuiWidget {
 
 	@Override
 	public int getWidth() {
-		return 34;
+		return 42;
 	}
 
 	@Override
 	public int getHeight() {
-		return 34;
+		return 42;
 	}
 
 	@Override
 	public void renderGui(GuiRenderState rs) {
 		ShaderManager.solidProgram.use();
 		ShaderManager.solidProgram.loadRenderState(rs);
-		Tessellator.instance.getBuffer().setColor(0xFF808080);
-		Tessellator.instance.getBuffer().addQuadVertex(0, 0, 0);
-		Tessellator.instance.getBuffer().addQuadVertex(0, 34, 0);
-		Tessellator.instance.getBuffer().addQuadVertex(34, 34, 0);
-		Tessellator.instance.getBuffer().addQuadVertex(34, 0, 0);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		Tessellator.instance.getBuffer().setColor(isHovered ? 0x60FFFFFF : 0x30FFFFFF);
+		Tessellator.instance.getBuffer().addQuadVertex(1, 1, 0);
+		Tessellator.instance.getBuffer().addQuadVertex(1, 41, 0);
+		Tessellator.instance.getBuffer().addQuadVertex(41, 41, 0);
+		Tessellator.instance.getBuffer().addQuadVertex(41, 1, 0);
 		Tessellator.instance.draw();
 		Tessellator.instance.getBuffer().setColor(0xFFFFFFFF);
+		GL11.glDisable(GL11.GL_BLEND);
+		/*Tessellator.instance.getBuffer().setColor(0xFFFFFFFF);
 		Tessellator.instance.getBuffer().addVertex(1, 1, 0);
 		Tessellator.instance.getBuffer().addVertex(1, 34, 0);
 		Tessellator.instance.getBuffer().addVertex(1, 34, 0);
@@ -52,12 +58,12 @@ public class GuiWidgetInventoryCell extends GuiWidget {
 		Tessellator.instance.getBuffer().addVertex(34, 1, 0);
 		Tessellator.instance.getBuffer().addVertex(34, 1, 0);
 		Tessellator.instance.getBuffer().addVertex(1, 1, 0);
-		Tessellator.instance.draw(GL11.GL_LINES);
+		Tessellator.instance.draw(GL11.GL_LINES);*/
 
 		ItemStack stack = container.getItemInSlot(slot);
 		GuiRenderState rs2 = new GuiRenderState(rs);
 		rs2.viewMatrix.mul(rs2.modelMatrix);
-		rs2.viewMatrix.translate(1f, 1f, 0f);
+		rs2.viewMatrix.translate(5f, 5f, 0f);
 		rs2.modelMatrix.identity();
 
 		if (stack.item != null && stack.count > 0) {
@@ -79,6 +85,7 @@ public class GuiWidgetInventoryCell extends GuiWidget {
 	@Override
 	public void handleMouseEvent(MouseEvent event) {
 		super.handleMouseEvent(event);
+		isHovered = event.isHovered();
 		if (event.isHovered()) {
 			if (event.getButtonState()) {
 				if (event.getButton() == 0) {
