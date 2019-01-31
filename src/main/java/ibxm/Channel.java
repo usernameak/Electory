de439e20562355a0e211d72641647ce294aa319a
usernameak <sitemakers.3dn.ru@gmail.com>
2019-01-31 19:50:59 +0200

    test 4

--- src/main/java/ibxm/Channel.java
+++ src/main/java/ibxm/Channel.java
@@ -396,10 +396,10 @@ public class Channel {
 	private void tonePortamento() {
 		if( period > 0 ) {
 			if( period < portaPeriod ) {
-				period += (tonePortaParam & 0xF);
+				period += (tonePortaParam) << 2;
 				if( period > portaPeriod ) period = portaPeriod;
-			} else {
-				period -= (tonePortaParam & 0xF);
+			} else if(period > portaPeriod) {
+				period -= (tonePortaParam) << 2;
 				if( period < portaPeriod ) period = portaPeriod;
 			}
 		}
